3829e978879b8ea5d5afc97514e9242da9144cc1
jiayu <jiayuji@elpuig.xeill.net>
2019-10-28 16:00:54 +0100

    17

--- src/com/company/ObjectStreamExample17.java
+++ src/com/company/ObjectStreamExample17.java
@@ -1,4 +1,22 @@
 package com.company;
 
+import java.io.Serializable;
+
 public class ObjectStreamExample17 {
+
+    public class Persona implements Serializable {
+        private String nom;
+
+        public String getNom() {
+            return nom;
+        }
+
+        public void setNom(String nom) {
+            this.nom = nom;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
